3ffa94be104440c25669deb10bde7ea2faee7553
Ethan Travasso <flameboyepic@gmail.com>
2023-11-19 22:00:31 +0000

    added a jump featuer

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ pygame.display.set_caption('Main Window')# Window name
 
 x, y = 50,50# Place where the character is created
 width,height = 40,60# Character size
-velo = 10# Character velocity
+velo = 5# Character velocity
 
 run = True
+jump = False
+jumpHeight = 10
 
 while run:# Mainloop
-    pygame.time.delay(100)# Equivilent of a tick in mc. In MS.
+    pygame.time.delay(50)# Equivilent of a tick in mc. In MS.
     # Events = Keyboard/mouse etc
 
     for event in pygame.event.get(): # Keyboard/mouse movements that have occured in this tick
@@ -28,13 +30,33 @@ while run:# Mainloop
     if keys[pygame.K_RIGHT] and x < winWidth - width - velo:
         # Character would otherwise be just 1 character outside the width
         x += velo
-    if keys[pygame.K_UP] and y > velo:
-        y -= velo
-    if keys[pygame.K_DOWN] and y < 500 - height - velo:
-        y += velo
+    if (not jump):
+            
+        if keys[pygame.K_UP] and y > velo:
+            y -= velo
+        if keys[pygame.K_DOWN] and y < 500 - height - velo:
+            y += velo
+        if keys[pygame.K_SPACE]:
+            jump = True
+        '''Uses a quadratic equation, f(k), to move the character in an arc
+        jump is 20 iterations long. for each iteration N, move f(N) positions in the y
+        direction. For the first half, move it upwards and in the second half move it back
+        down (hence why nec becomes negative)
+        '''
 
+    else:
+        if jumpHeight >= -10:
+            #neg = 1 if jumpHeight >= -20 else -1
+            neg = 1
+            if jumpHeight < 0:
+                neg = -1
+            y -= (jumpHeight **2) * 0.25 * neg
+            jumpHeight -=1
+        else:
+            jump = False
+            jumpHeight = 10
+            
     window.fill((0,0,0)) #Gets rid of the old stuff?
-    
     pygame.draw.rect(window, (255,0,0), (x,y,width,height))# Creates a rectangle. More in website
     pygame.display.update()# Refresh screen. Same as C#
 
---
+++ sandbox.py
@@ -0,0 +1,4 @@
+jump = 10
+
+neg = int(jump<0)
+print(neg)
